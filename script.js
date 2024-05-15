c39c31ba5de6bc4769824a0957d54fc7a9f585aa
Murodil <asilbeksherqulov89@gmail.com>
2024-05-15 22:14:17 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// alert("Salom.")
+// alert("Sizga qaysi ilova kerak?")
+// alert("facebook,twitter,whatsap yoki boshqa ilovani tanlang va o'sha ilovaga kiring,")
+// alert("O'sha tanlagan ilovangizga kirmasa,")
+// alert(" oldindan uzur so'rab qolamiz.")
